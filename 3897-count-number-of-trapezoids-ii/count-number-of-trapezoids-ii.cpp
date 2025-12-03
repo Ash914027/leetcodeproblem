cc848204153fca4d55a8aa45ba108b9773d65b55
Ashish Chauhan <119170180+Ash914027@users.noreply.github.com>
2025-12-03 19:56:02 +0530

    Time: 1795 ms (78.70%) | Memory: 547.3 MB (54.44%) - LeetSync

--- 3897-count-number-of-trapezoids-ii/count-number-of-trapezoids-ii.cpp
+++ 3897-count-number-of-trapezoids-ii/count-number-of-trapezoids-ii.cpp
@@ -1,97 +1,81 @@
-#include <bits/stdc++.h>
-#include <ranges>
-#include <tuple>
-using namespace std;
-
-
 class Solution {
 public:
-    static const long long DEFAULT_SCALE = 1000000000LL;
-    long long encodeFraction(long long x, long long y, long long bias = 2000, long long scale = DEFAULT_SCALE) {
-        long long gcd = __gcd(x, y);
-        x /= gcd;
-        y /= gcd;
-        return x + bias + (y + bias) * scale;
+    using ll = long long;
+    
+    static constexpr int SHIFT = 3000;
+    
+    int encodePair(int a, int b) {
+        return ((a + SHIFT) << 13) ^ (b + SHIFT);
     }
-
-    long long getAngle(double x1, double y1, double x2, double y2) {
-        long long x = x2 - x1;
-        long long y = y2 - y1;
-        return encodeFraction(x, y);
-    }
-
-    long long getIntersection(double x1, double y1, double x2, double y2) {
-        if (y1 == y2) {
-            return encodeFraction(DEFAULT_SCALE / 10, y1);
+    
+    int myGcd(int a, int b) {
+        if (a < 0) a = -a;
+        if (b < 0) b = -b;
+        while (b != 0) {
+            int t = a % b;
+            a = b;
+            b = t;
         }
-        // find intersection with x axis
-        // y - y1 = (y2 - y1) / (x2 - x1) * (x - x1)
-        // when y = 0
-        // x = x1 - y1 * (x2 - x1) / (y2 - y1)
-        // Make sure there are no error while dividing intergers
-        return encodeFraction(x1 * (y2 - y1) - y1 * (x2 - x1), y2 - y1, 0);
+        return a;
     }
-
+    
+    ll countPairs(const unordered_map<int, unordered_map<int,int>>& mp) {
+        ll res = 0;
+        for (const auto &outer : mp) {
+            const auto &inner = outer.second;
+            ll sum = 0, sumSq = 0;
+            for (auto &kv : inner) {
+                ll c = kv.second;
+                sum   += c;
+                sumSq += c * c;
+            }
+            res += (sum * sum - sumSq) / 2;
+        }
+        return res;
+    }
+    
     int countTrapezoids(vector<vector<int>>& points) {
-        // m[angle][intersection][length] = count
-        unordered_map<long long, unordered_map<long long, unordered_map<long long, long long>>> m;
-        for (int i1 = 0; i1 < points.size(); i1++) {
-            for (int i2 = i1 + 1; i2 < points.size(); i2++) {
-                long long x1 = points[i1][0], y1 = points[i1][1];
-                long long x2 = points[i2][0], y2 = points[i2][1];
-                long long angle = getAngle(x1, y1, x2, y2);
-                long long intersection = getIntersection(x1, y1, x2, y2);
-                long long len = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
-                m[angle][intersection][len]++;
+        int n = (int)points.size();
+        
+        unordered_map<int, unordered_map<int,int>> bySlope;
+        unordered_map<int, unordered_map<int,int>> byVector;
+        
+        bySlope.reserve(n * n);
+        byVector.reserve(n * n);
+        
+        for (int i = 0; i < n; ++i) {
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+            for (int j = i + 1; j < n; ++j) {
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+                
+                int dx = x2 - x1;
+                int dy = y2 - y1;
+                
+                if (dx < 0 || (dx == 0 && dy < 0)) {
+                    dx = -dx;
+                    dy = -dy;
+                }
+                
+                int g = myGcd(dx, dy);
+                int ux = dx / g;
+                int uy = dy / g;
+                
+                int lineId = ux * y1 - uy * x1;
+                
+                int slopeKey  = encodePair(ux, uy);
+                int vectorKey = encodePair(dx, dy);
+                
+                bySlope[slopeKey][lineId]  += 1;
+                byVector[vectorKey][lineId] += 1;
             }
-        }
-        long long trapezoidCount = 0;
-        long long parallelogramCount = 0;
-        for (auto it1 = m.begin(); it1 != m.end(); it1++) {
-            auto [count, parallelogram] = countTrapezoids2(it1->second);
-            trapezoidCount += count;
-            parallelogramCount += parallelogram;
-        }
-        return trapezoidCount + parallelogramCount / 2;
-    }
-
-    pair<long long, long long> countTrapezoids2(unordered_map<long long, unordered_map<long long, long long>>& groupedByIntersectionM) {
-        long long edgeCount = 0;
-        for (auto& [intersection, groupedByLen] : groupedByIntersectionM) {
-            for (auto& [len, c] : groupedByLen) {
-                edgeCount += c;
-            }
-        }
-        long long trapezoidCount = 0;
-        for (auto& [intersection, groupedByLen] : groupedByIntersectionM) {
-            int count = 0;
-            for (auto& [len, c] : groupedByLen) {
-                count += c;
-            }
-            trapezoidCount += count * (edgeCount - count);
-            edgeCount -= count;
         }
         
-        // Hanle parallelogram
-        // groupByLen[length][intersection]
-        unordered_map<long long, unordered_map<long long, long long>> groupByLenM;
-        for (auto& [intersection, groupedByLen] : groupedByIntersectionM) {
-            for (auto& [len, c] : groupedByLen) {
-                groupByLenM[len][intersection] += c;
-            }
-        }
-        long long parallelogramCount = 0;
-        for (auto& [len, groupByIntersection] : groupByLenM) {
-            int total = 0;
-            for (auto& [intersection, c] : groupByIntersection) {
-                total += c;
-            }
-            for (auto& [intersection, c] : groupByIntersection) {
-                parallelogramCount += c * (total - c);
-                total -= c;
-            }
-        }
-
-        return {trapezoidCount - parallelogramCount, parallelogramCount};
+        ll withParallel = countPairs(bySlope);
+        ll parallelogramTwice = countPairs(byVector);
+        
+        ll ans = withParallel - parallelogramTwice / 2;
+        return (int)ans;
     }
 };
